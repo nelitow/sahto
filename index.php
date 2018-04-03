f40eb102d0e5e64c4ed4329197f6ec5d8208d0c5
Nelito <ngvjqp@gmail.com>
2018-04-02 23:50:28 -0300

    cu

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>s.ahto</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1>s.ahto</h1>
+				<p>I'ts a <b>s</b>hort <b>ahto</b>
+			</header>
+
+			<form id="signup-form" method="post" action="#">
+				<input type="text" name="url" id="url" placeholder="URL" />
+				<input type="text" name="name" id="name" placeholder="Nome (não obrigatório)" />
+				<input type="submit" value="Criar" />
+			</form>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="https://instagram.com/ahtocoletivo" target="_blank" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="https://api.whatsapp.com/send?phone=5548998586762" class="icon fa-whatsapp"><span class="label">WhatsApp</span></a></li>
+					<li>Sua URL: adasdasdd</li>
+				</ul>
+				<ul class="copyright">
+					<li>Credits: <a href="http://html5up.net">HTML5 UP</a></li>
+				</ul>
+			</footer>
+
+		<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
